7dcd133a588f235c66550fade606036096150258
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-12 13:24:06 +0545

    files

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+from pathlib import Path
+
+
+PARENT_DIR = r'C:\Users\sxoxgxi\Downloads'
+IMAGES_FILES = [
+    '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png',
+    '.gif', '.webp', '.svg', '.apng', '.avif'
+]
+
+VIDEOS_FILES = [
+    '.webm', '.mts', '.m2ts', '.ts', '.mov',
+    '.mp4', '.m4p', '.m4v', '.mxf'
+]
+
+AUDIO_FILE = [
+    '.3ga', '.aac', '.ac3', '.aif', '.aiff',
+    '.alac', '.amr', '.ape', '.au', '.dss',
+    '.flac', '.flv', '.m4a', '.m4b', '.m4p',
+    '.mp3', '.mpga', '.ogg', '.oga', '.mogg',
+    '.opus', '.qcp', '.tta', '.voc', '.wav',
+    '.wma', '.wv'
+]
+
+DOCUMENTS_FILES = [
+    '.doc','.docx','.log','.msg','.odt',
+    '.pages','.rtf','.tex','.txt','.wpd',
+    '.wps','.indo','.pct','.pdf'
+]
+
+DATA_FILES = [
+    '.csv','.dat','.ged','.key','.keychain',
+    '.ppt','.pptx','.sdf','.tar','.tax2016',
+    '.tax2020', '.tax2021', '.vcf', '.xml'
+]
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == '__main__':
+    main()
